787ce51611f9f77ae628bcf1202daad8b7145b7d
Joe Davis <joedavis29@gmail.com>
2023-03-20 00:52:13 -0400

    fix local

--- apps/expo/src/utils/trpc.tsx
+++ apps/expo/src/utils/trpc.tsx
@@ -26,10 +26,12 @@ const getBaseUrl = () => {
    * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
    * you don't have anything else running on it, or you'd have to change it.
    */
-  const localhost = Constants.manifest?.debuggerHost?.split(":")[0];
-  if (!localhost)
-    throw new Error("failed to get localhost, configure it manually");
-  return `http://${localhost}:3000`;
+  if (!Constants.manifest) {
+    throw new Error('no manifest in Constatns')
+  }
+  const { logUrl } = Constants.manifest
+  const localhost = logUrl?.split(":")[1];
+  return `http:${localhost}:3000`;
 };
 
 export const TRPCProvider: React.FC<{
