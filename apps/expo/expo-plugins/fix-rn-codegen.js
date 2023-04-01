0e000202c136085a3972457e42dbc5a3383d0619
Joe Davis <joedavis29@gmail.com>
2023-03-31 20:10:15 -0400

    fix rn-codegen

--- apps/expo/expo-plugins/fix-rn-codegen.js
+++ apps/expo/expo-plugins/fix-rn-codegen.js
@@ -1,26 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { withDangerousMod, withPlugins } = require("@expo/config-plugins");
+const { ExpoConfig } = require("@expo/config-types");
 const {
   mergeContents,
 } = require("@expo/config-plugins/build/utils/generateCode");
-const fs = require("fs");
-const path = require("path");
-
-async function readFileAsync(path) {
-  return fs.promises.readFile(path, "utf8");
-}
-
-async function saveFileAsync(path, content) {
-  return fs.promises.writeFile(path, content, "utf8");
-}
+const { readFileSync, writeFileSync } = require("fs");
+const { resolve } = require("path");
 
 const withFixedDeploymentTarget = (c) => {
   return withDangerousMod(c, [
     "ios",
     async (config) => {
-      const file = path.join(config.modRequest.platformProjectRoot, "Podfile");
-      const contents = await readFileAsync(file);
-      await saveFileAsync(file, fixDeploymentTarget(contents));
+      const file = resolve(config.modRequest.platformProjectRoot, "Podfile");
+      const contents = readFileSync(file, { encoding: "utf-8" });
+      writeFileSync(file, fixDeploymentTarget(contents));
       return config;
     },
   ]);
@@ -31,10 +24,12 @@ function fixDeploymentTarget(src) {
     tag: `rn-fix-deployment-target`,
     src,
     newSrc: `
-   installer.pods_project.targets.each do |target|
-        target.build_configurations.each do |config|
-               config.build_settings['SWIFT_VERSION'] = '5.0'
-               config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
+  installer.pods_project.targets.each do |target|
+    if target.to_s === 'React-Codegen'
+      target.build_configurations.each do |config|
+        config.build_settings['SWIFT_VERSION'] = '5.0'
+        config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
+      end
     end
   end
 `,
