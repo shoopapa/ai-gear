7221d4019be1c38ceb76e264c4143604d4c56f85
Joe Davis <joedavis29@gmail.com>
2023-09-03 13:55:42 -0400

    make match

--- apps/expo/src/components/sessions-list.tsx
+++ apps/expo/src/components/sessions-list.tsx
@@ -5,6 +5,8 @@ import { List, ActivityIndicator } from 'react-native-paper';
 import { timeAgo } from '../utils/time-ago';
 
 import { useMyTheme } from '../perfereneces';
+import { trpc } from '../utils/trpc';
+
 
 type SessionListProps = {
   navigate: (id: string) => void;
@@ -22,6 +24,7 @@ export const SessionList = ({ navigate, recordings, isFetching }: SessionListPro
   if (!recordings || isFetching) {
     return (
       <View className='mt-1 h-full'>
+        <Text className='m-3'>10 Most Recent Sessions</Text>
         <ActivityIndicator animating={true} color={theme.colors.primary} />
       </View>
     );
@@ -29,29 +32,31 @@ export const SessionList = ({ navigate, recordings, isFetching }: SessionListPro
 
   return (
     <View>
-      <ScrollView className='text-black'>
-        {recordings.map((s) => {
-          let t = 'No Create at'
-          if (s.createdAt) {
-            t = timeAgo.format(new Date(s.createdAt));
-          }
-          return (
-            <List.Item
-              className='mx-2 my-0 py-1 px-1'
-              theme={theme}
-              titleStyle={{ color: 'black' }}
-              descriptionStyle={{ color: 'black' }}
-              onPress={() => navigate(s.id)}
-              key={s.id}
-              title={s.name}
-              description={t}
-              left={() => (
-                <List.Icon style={{ padding: 0, margin: 0 }} color='black' icon="run" />
-              )}
-            />
-          );
-        })}
-      </ScrollView>
+      <View>
+        <ScrollView className='text-black'>
+          {recordings.map((s) => {
+            let t = 'No Create at'
+            if (s.createdAt) {
+              t = timeAgo.format(new Date(s.createdAt));
+            }
+            return (
+              <List.Item
+                className='mx-2 my-0 py-1 px-1'
+                theme={theme}
+                titleStyle={{ color: 'black' }}
+                descriptionStyle={{ color: 'black' }}
+                onPress={() => navigate(s.id)}
+                key={s.id}
+                title={s.name}
+                description={t}
+                left={() => (
+                  <List.Icon style={{ padding: 0, margin: 0 }} color='black' icon="run" />
+                )}
+              />
+            );
+          })}
+        </ScrollView>
+      </View>
     </View>
   );
 }
