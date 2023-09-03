616967fb94c2b1175be1a07297561764e6d362e0
Joe Davis <joedavis29@gmail.com>
2023-09-03 16:16:08 -0400

    support page

--- apps/marketing/components/contact.tsx
+++ apps/marketing/components/contact.tsx
@@ -1,6 +1,4 @@
 "use client"
-// components/ContactForm.js
-
 import React, { useState } from 'react';
 
 const ContactForm = () => {
@@ -14,7 +12,7 @@ const ContactForm = () => {
     agreeToPrivacyPolicy: false,
   });
 
-  const handleInputChange = (e) => {
+  const handleInputChange = (e: any) => {
     const { name, value, type, checked } = e.target;
     const inputValue = type === 'checkbox' ? checked : value;
 
@@ -24,87 +22,74 @@ const ContactForm = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     // Here, you can implement the logic to send the form data to your backend or support email
     console.log(formData); // For demonstration purposes, log the form data
   };
 
   return (
-    <div className="max-w-4xl mx-auto bg-gray-900 p-6 rounded-lg shadow-md" data-aos="fade-up" data-aos-delay="400">
+    <div className="max-w-4xl mx-auto bg p-6" data-aos="fade-up" data-aos-delay="400">
       <form onSubmit={handleSubmit}>
         <div className="flex flex-wrap -mx-4 mb-4">
           <div className="w-full md:w-1/2 px-4 mb-4">
-            <label htmlFor="firstName" className="block text-gray-200 text-sm font-bold mb-1">First Name *</label>
+            <label className="block text-gray-200 text-sm font-bold mb-1">First Name *</label>
             <input
               type="text"
               id="firstName"
               name="firstName"
               value={formData.firstName}
               onChange={handleInputChange}
-              className="w-full border border-gray-300 rounded px-3 py-2 bg-gray-700 text-gray-200 focus:outline-none focus:border-indigo-500"
+              className="w-full border border-gray-00 rounded px-3 py-2 bg-transparent text-gray-200 focus:outline-none focus:border-indigo-500"
               required
             />
           </div>
           <div className="w-full md:w-1/2 px-4 mb-4">
-            <label htmlFor="lastName" className="block text-gray-200 text-sm font-bold mb-1">Last Name *</label>
+            <label className="block text-gray-200 text-sm font-bold mb-1">Last Name *</label>
             <input
               type="text"
               id="lastName"
               name="lastName"
               value={formData.lastName}
               onChange={handleInputChange}
-              className="w-full border border-gray-300 rounded px-3 py-2 bg-gray-700 text-gray-200 focus:outline-none focus:border-indigo-500"
+              className="w-full border border-gray-00 rounded px-3 py-2 bg-transparent text-gray-200 focus:outline-none focus:border-indigo-500"
               required
             />
           </div>
         </div>
         <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-200 text-sm font-bold mb-1">Email *</label>
+          <label className="block text-gray-200 text-sm font-bold mb-1">Email *</label>
           <input
             type="email"
             id="email"
             name="email"
             value={formData.email}
             onChange={handleInputChange}
-            className="w-full border border-gray-300 rounded px-3 py-2 bg-gray-700 text-gray-200 focus:outline-none focus:border-indigo-500"
+            className="w-full border border-gray-00 rounded px-3 py-2 bg-transparent text-gray-200 focus:outline-none focus:border-indigo-500"
             required
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="subject" className="block text-gray-200 text-sm font-bold mb-1">Subject *</label>
+          <label className="block text-gray-200 text-sm font-bold mb-1">Subject *</label>
           <input
             type="text"
             id="subject"
             name="subject"
             value={formData.subject}
             onChange={handleInputChange}
-            className="w-full border border-gray-300 rounded px-3 py-2 bg-gray-700 text-gray-200 focus:outline-none focus:border-indigo-500"
+            className="w-full border border-gray-500 rounded px-3 py-2 bg-transparent text-gray-200 focus:outline-none focus:border-indigo-500"
             required
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="country" className="block text-gray-200 text-sm font-bold mb-1">Country</label>
-          <select
-            id="country"
-            name="country"
-            value={formData.country}
-            onChange={handleInputChange}
-            className="w-full border border-gray-300 rounded px-3 py-2 bg-gray-700 text-gray-200 focus:outline-none focus:border-indigo-500"
-          >
-            <option value="United States">United States</option>
-            {/* Add other country options here */}
-          </select>
-        </div>
-        <div className="mb-4">
-          <label htmlFor="message" className="block text-gray-200 text-sm font-bold mb-1">Message</label>
+          <label className="block text-gray-200 text-sm font-bold mb-1">Message</label>
           <textarea
             id="message"
             name="message"
             value={formData.message}
             onChange={handleInputChange}
-            className="w-full border border-gray-300 rounded px-3 py-2 bg-gray-700 text-gray-200 focus:outline-none focus:border-indigo-500"
-            rows="4"
+            className="w-full border border-gray-00 rounded px-3 py-2 bg-transparent text-gray-200 focus:outline-none focus:border-indigo-500"
+            rows={4}
             required
           ></textarea>
         </div>
@@ -130,8 +115,8 @@ const ContactForm = () => {
             Send
           </button>
         </div>
-      </form>
-    </div>
+      </form >
+    </div >
   );
 };
 
