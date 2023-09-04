4270d17d7ada90a0d4f195b83d27b854ab8ec86f
jdavis <45986634+shoopapa@users.noreply.github.com>
2023-09-04 18:19:52 -0400

    Update mobile-menu.tsx

--- apps/marketing/components/ui/mobile-menu.tsx
+++ apps/marketing/components/ui/mobile-menu.tsx
@@ -61,7 +61,7 @@ export default function MobileMenu() {
       >
         <ul className="bg-gray-800 px-4 py-2">
           <li>
-            <Link href="/signin" className="flex font-medium w-full text-purple-100 hover:text-gray-400 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>
+            <Link href="/support" className="flex font-medium w-full text-purple-100 hover:text-gray-400 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>
               Support
             </Link>
           </li>
