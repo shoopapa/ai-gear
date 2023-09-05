de0c3e05d9b09ba6ae665a258c03484dfda46b0a
Joe Davis <joedavis29@gmail.com>
2023-09-05 18:35:58 -0400

    inc version

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -11,7 +11,7 @@ const defineConfig = (): ExpoConfig => ({
   name: "AI Gear",
   slug: "ai-gear",
   jsEngine: "hermes",
-  version: "1.3.0",
+  version: "1.4.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
