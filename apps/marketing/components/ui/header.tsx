bc7c8a41a2dfe3c39c8ed4335a1de10d022a0ded
Joe Davis <joedavis29@gmail.com>
2023-09-05 18:33:05 -0400

    fix links

--- apps/marketing/components/ui/header.tsx
+++ apps/marketing/components/ui/header.tsx
@@ -32,14 +32,14 @@ export default function Header() {
             <ul className="flex grow justify-end flex-wrap items-center">
               <li>
                 <Link
-                  href="/signin"
+                  href="https://app.ai-gear.com/sign-in"
                   className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
                 >
                   Sign in
                 </Link>
               </li>
               <li>
-                <Link href="/signup" className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">
+                <Link href="https://app.ai-gear.com/sign-up" className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">
                   Sign up
                 </Link>
               </li>
--- apps/marketing/components/ui/mobile-menu.tsx
+++ apps/marketing/components/ui/mobile-menu.tsx
@@ -66,13 +66,13 @@ export default function MobileMenu() {
             </Link>
           </li>
           <li>
-            <Link href="/signin" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>
+            <Link href="https://app.ai-gear.com/sign-in" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>
               Sign in
             </Link>
           </li>
           <li>
             <Link
-              href="/signup"
+              href="https://app.ai-gear.com/sign-up"
               className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out" onClick={() => setMobileNavOpen(false)}
             >
               Sign up
